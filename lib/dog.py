e08bb0f804eeb55d0f3a3a626b87d6ab5587c304
stella-mumbi <stella.mumbi@student.moringaschool.com>
2024-03-03 19:59:57 +0300

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
+# class Dog:
+#     pass
+
+# lib/dog.py
+
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
